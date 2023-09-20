9879c0b79d63da8799035190eaec191fa49a415e
Eakond1 <143996703+Eakond1@users.noreply.github.com>
2023-09-20 21:29:22 +0300

    Add files via upload

---
+++ abs.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../middle.h"
+using namespace std;
+int itc_abs(int num){
+    if (num<0){
+        num=num*(-1);
+    }
+    return num;
+}
---
+++ pow.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "../middle.h"
+using namespace std;
+double itc_pow(int num, int step){
+    double a = num;
+    if (step==0){
+        return 1;
+    }
+    else if (step>0){
+        for (int i=1; i < step; i++){
+            a=a*num;
+        }
+        return a;
+    }
+
+    if (step==0){
+        return 0;
+    }
+    else {
+        for(int i=-1; i>step; i--){
+            a=a*num;
+        }
+        return 1/a;
+    }
+}
